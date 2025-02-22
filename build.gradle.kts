3af60894bd7b3e90f78d526cb0e86825002817ba
Damien Nichols <Damienlovemusic@gmail.com>
2025-02-22 07:09:50 -0700

    more fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@
 
 // Plugins
 plugins {
-    id("com.android.application") version "8.8.1" apply false
+    id("com.android.application") version "8.8.0-alpha05" apply false
     id("com.android.library") version "8.8.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     alias(libs.plugins.google.gms.google.services) apply false
